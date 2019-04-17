54d66f21f0d0360ab7028c050c0a15dce6cfd653
Ksenia Lashchinovskaya <ksLa1998@mail.ru>
2019-04-17 12:24:03 +0300

    add lock syncronization

--- ThreadSyncTask/ThreadSyncTask/Program.cs
+++ ThreadSyncTask/ThreadSyncTask/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ThreadSyncTask.SyncClasses;
 
 namespace ThreadSyncTask
 {
@@ -10,6 +11,10 @@ namespace ThreadSyncTask
     {
         static void Main(string[] args)
         {
+            var lockSync = new LockSync(3);
+            lockSync.StartSync();
+
+            Console.ReadLine();
         }
     }
 }
